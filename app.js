e6f2d2c7b801c8f037d7327e771cd22c7a79bbf1
woodb58 <woodb58@gmail.com>
2024-01-21 23:23:44 -0600

    added nav menu for mobile devices

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("HI")
+const hamburger = document.getElementById("hamburger")
+const nav = document.getElementById("nav")
+
+hamburger.addEventListener('click', ()=>{
+    hamburger.classList.toggle('active')
+    nav.classList.toggle('active')
+})
+
+document.querySelectorAll(".nav-link").forEach(e => e.addEventListener('click', ()=> {
+    hamburger.classList.remove('active')
+    nav.classList.remove('active')
+}))
